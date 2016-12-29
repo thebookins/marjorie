b83176815ef7b49c7a87da0779ad075e50e796f4
Paul Dickens <dickens.paul@gmail.com>
2016-12-29 12:00:50 +1100

    fix typo

--- pump/index.js
+++ pump/index.js
@@ -14,7 +14,7 @@ module.exports = (t1d) => {
 
   setInterval(() => {
     timestamp++;
-    insulinDeficit += basal_rate_U_per_hour / 3600;
+    insulinDeficit_U += basal_rate_U_per_hour / 3600;
     if (insulinDeficit_U >= 0.05) {
       bolus(0.05);
       insulinDeficit_U -= 0.05;
