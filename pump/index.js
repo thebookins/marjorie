7d609b3cdbc2b7b87459d485fe7fa293201b4b9b
Paul Dickens <dickens.paul@gmail.com>
2016-12-29 11:57:14 +1100

    add basal rate

--- pump/index.js
+++ pump/index.js
@@ -8,12 +8,20 @@ module.exports = (t1d) => {
   // private data
   var reservoirUnits = 300;
   var timestamp = 0;
+  var insulinDeficit_U = 0;
+
+  var basal_rate_U_per_hour = 10;
 
   setInterval(() => {
+    timestamp++;
+    insulinDeficit += basal_rate_U_per_hour / 3600;
+    if (insulinDeficit_U >= 0.05) {
+      bolus(0.05);
+      insulinDeficit -= 0.05;
+    }
     if (!(timestamp % 300)) { // every five minutes
       eventEmitter.emit('reservoir', reservoirUnits);
     }
-    timestamp++;
   }, 1000);
 
   return {
