8d84fef0f93d65b62e558cf8df5de09778e1c453
Paul Dickens <dickens.paul@gmail.com>
2016-12-30 12:48:11 +1100

    better messages

--- pump/index.js
+++ pump/index.js
@@ -32,7 +32,7 @@ module.exports = (t1d) => {
       eventEmitter.emit('reservoir', reservoirUnits);
     }
     mc.set('reservoir', 'test', function(err, val) {
-      console.log("set reservoir to " + reservoirUnits + ", with error " + err)
+//      console.log("set reservoir to " + reservoirUnits + ", with error " + err)
       mc.get('reservoir', function(val) {
         console.log("reservoirUnits = " + val);
       })
