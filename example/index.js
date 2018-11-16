1ed493c0ba85d33185d0ae1b2512d53b2cbc259a
Paul Dickens <dickens.paul@gmail.com>
2018-11-16 19:33:06 +1100

    add travis.yml

--- example/index.js
+++ example/index.js
@@ -1,5 +1,6 @@
-const t1d = require('./t1d/hovorka')(1);
-for (t = 0; t < 10; t++) {
+const t1d = require('..')(1);
+
+for (let t = 0; t < 10; t += 1) {
   console.log(`${t}, ${t1d.glucose}`);
   // this could return the time, or alternatively, we could
   // change to a stepTo function (or runUntil) with the time as a parameter
