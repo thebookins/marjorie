0a4f2c7da3930701cf2770f60e8f722054cadd57
Paul Dickens <dickens.paul@gmail.com>
2018-11-22 21:16:56 +1100

    various changes

---
+++ test/test-glucose.js
@@ -0,0 +1,31 @@
+const should = require('chai').should();
+const Glucose = require('../lib/hovorka/glucose');
+
+describe('Glucose', function() {
+//   describe('insulin', function() {
+//     it('should be zero with zero administration', function() {
+//       const insulin = Insulin(1);
+//
+//       for (let t = 0; t < 60; t += 1) {
+//         insulin.insulin.should.equal(0);
+//         insulin.step(0);
+//       }
+//     });
+//
+//     // NOTE: not sure about this test
+//     it('should peak around 55 min after a bolus', function() {
+//       const insulin = Insulin(1);
+//
+//       let peakTime = 0;
+//       let peakInsulin = insulin.insulin;
+//       for (let t = 0; t < 10 * 60; t += 1) {
+//         insulin.step(t === 0 ? 1000 : 0);
+//         if (insulin.insulin > peakInsulin) {
+//           peakInsulin = insulin.insulin;
+//           peakTime = t;
+//         }
+//       }
+//       peakTime.should.be.within(45, 65);
+//     })
+//   })
+});
