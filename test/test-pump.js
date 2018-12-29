b09b6299b1d1a42b4007eb231fac804bc9a02a1e
Paul Dickens <dickens.paul@gmail.com>
2018-12-30 00:05:45 +1100

    work on pump

---
+++ test/test-pump.js
@@ -0,0 +1,10 @@
+const should = require('chai').should();
+const Pump = require('../lib/pump');
+
+describe('Pump', function() {
+  describe('basal', function() {
+    it('should set temporary basal rates');
+    it('should cancel temporary basal rates');
+    it('should notify on basal rate transitions');
+  });
+});
