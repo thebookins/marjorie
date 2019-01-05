6c4e3fb96da5776ddd4751ab5515b917087b6bb5
Paul Dickens <dickens.paul@gmail.com>
2019-01-05 23:19:19 +1100

    add dir pwd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
+const PWD = require('./lib/pwd/hovorka');
 const Pump = require('./lib/pump');
-const Model = require('./lib/hovorka'); // TODO work out better name
 
-module.exports = { Pump, Model };
+module.exports = { PWD, Pump };
