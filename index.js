1d6ab37a50869e014cd4a986ebf2c9dc000fe437
Paul Dickens <dickens.paul@gmail.com>
2019-02-27 21:31:23 +1100

    export CGM

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const PWD = require('./lib/pwd/hovorka');
 const Pump = require('./lib/pump');
+const CGM = require('./lib/cgm');
 
-module.exports = { PWD, Pump };
+module.exports = { PWD, Pump, CGM };
